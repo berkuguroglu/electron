88eee847611066c7a5986e49d950777260fb9a11
Samuel Attard <sattard@slack-corp.com>
2019-02-07 11:32:23 -0800

    refactor: convert more files to typescript

--- lib/browser/init.ts
+++ lib/browser/init.ts
@@ -49,10 +49,12 @@ process.on('uncaughtException', function (error) {
   }
 
   // Show error in GUI.
-  const dialog = require('electron').dialog
-  const stack = error.stack ? error.stack : `${error.name}: ${error.message}`
-  const message = 'Uncaught Exception:\n' + stack
-  dialog.showErrorBox('A JavaScript error occurred in the main process', message)
+  import('electron')
+    .then(({ dialog }) => {
+      const stack = error.stack ? error.stack : `${error.name}: ${error.message}`
+      const message = 'Uncaught Exception:\n' + stack
+      dialog.showErrorBox('A JavaScript error occurred in the main process', message)
+    })
 })
 
 // Emit 'exit' event on quit.
@@ -192,10 +194,13 @@ app.on('window-all-closed', () => {
   }
 })
 
-const { setDefaultApplicationMenu } = require('@electron/internal/browser/default-menu')
-
-// Create default menu.
-app.once('ready', setDefaultApplicationMenu)
+Promise.all([
+  import('@electron/internal/browser/default-menu'),
+  app.whenReady
+]).then(([{ setDefaultApplicationMenu }]) => {
+  // Create default menu
+  setDefaultApplicationMenu()
+})
 
 if (packagePath) {
   // Finally load app's main.js and transfer control to C++.
